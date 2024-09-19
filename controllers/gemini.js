5576b8c38750ac543c8c3e76d6f59bc55de6d16f
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-19 23:34:41 +0700

    improve code typing

--- controllers/gemini.js
+++ controllers/gemini.js
@@ -1,4 +1,4 @@
-const model = require('../utils/gemini');
+const model = require("../utils/gemini");
 
 const componentGenerator = async (req, res, next) => {
     const { input } = req.body;
@@ -6,11 +6,11 @@ const componentGenerator = async (req, res, next) => {
         const prompt = `write me jsx component that create ${input} with tailwind for my react project. Give me code only!`;
         const result = await model.generateContent(prompt);
 
-        return res.send(result.response.text())
+        return res.send(result.response.text());
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 const codeConverter = async (req, res, next) => {
     const { before, after, input } = req.body;
@@ -18,22 +18,22 @@ const codeConverter = async (req, res, next) => {
         const prompt = `change this code from ${before} programinglanguage into ${after} programing language. Code: ${input}. Give me code only!`;
         const result = await model.generateContent(prompt);
 
-        return res.send(result.response.text())
+        return res.send(result.response.text());
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 const codeSolver = async (req, res, next) => {
     const { input, errorMessage } = req.body;
     try {
         const prompt = `my code = ${input}. error message = ${errorMessage}. explain why this erro can happen and give me solution`;
         const result = await model.generateContent(prompt);
 
-        return res.send(result.response.text())
+        return res.send(result.response.text());
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 const guide = async (req, res, next) => {
     const { input } = req.body;
@@ -41,10 +41,10 @@ const guide = async (req, res, next) => {
         const prompt = `write me a guide with markdown format. guide of ${input} from the start until finished`;
         const result = await model.generateContent(prompt);
 
-        return res.send(result.response.text())
+        return res.send(result.response.text());
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
-module.exports = { componentGenerator, codeConverter, guide, codeSolver }
+module.exports = { componentGenerator, codeConverter, guide, codeSolver };
